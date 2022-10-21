3f1cd5d211b64e499fc5b8cb4766c473985c2851
Samandar000 <samandarnfh.2020@gmail.com>
2022-10-21 21:32:57 +0500

    code without def

--- send_photo.py
+++ send_photo.py
@@ -2,13 +2,11 @@ import requests
 
 TOKEN = '5643654386:AAGaxNP-8Kkwzi8Ko047p0BZBd3t6a0eIu4'
 # while True:
-def pair(Dog,Cat):
-    Dog = requests.get('https://random.dog/woof.json')
-    dog_data = Dog.json()
+Dog = requests.get('https://random.dog/woof.json')
+dog_data = Dog.json()
 
-    Cat = requests.get('https://aws.random.cat/meow')
-    cat_data = Cat.json()
-    return (dog_data,cat_data)
+Cat = requests.get('https://aws.random.cat/meow')
+cat_data = Cat.json()
 
 updates = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
 updates = updates.json()
@@ -19,20 +17,16 @@ text = updates['result'][-1]['message']['text']
 url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
 dog = 'dog' or 'DOG' or 'Dog'
 cat = 'cat' or 'CAT' or 'Cat'
-def get_url(dog_data,cat_data):
-    if text in dog:
-        img_url = dog_data.get('url')
-    if text in cat:
-        img_url = cat_data.get('url')
-    return img_url
-def dicti(img_url):
-    data = {
-            'chat_id':chat_id,
-            'photo':img_url,
-            'text':text
-        }
-    Dog = requests.post(url,data=data)
-    Cat = requests.post(url,data=data)
-    return Cat,Dog
-
-    
+if text in dog:
+    img_url = dog_data.get('url')
+if text in cat:
+    img_url = cat_data.get('url')
+data = {
+        'chat_id':chat_id,
+        'photo':img_url,
+        'text':text
+    }
+Dog = requests.post(url,data=data)
+Cat = requests.post(url,data=data)
+print(Dog.status_code)
+print(Cat.status_code)
