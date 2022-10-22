e2121188886b9dab5ce9c8a02e99552a52a20502
Samandar000 <samandarnfh.2020@gmail.com>
2022-10-22 11:56:13 +0500

    full code

--- send_photo.py
+++ send_photo.py
@@ -7,10 +7,19 @@ def get_updates(TOKEN):
     updates = updates.json()
     return updates
 
-def paramet_dog(Dog):
+def send_dog(chat_id):
     Dog = requests.get('https://random.dog/woof.json')
-    dog_data = Dog.json()
-    return dog_data 
+    dog_data = Dog.json()['url']
+    data = {
+        'chat_id':chat_id,
+        'photo':dog_data
+
+        }
+    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto',data=data)
+    
+
+
+
 def send_cat(chat_id):
     Cat = requests.get('https://aws.random.cat/meow')
     url= Cat.json()['file']
@@ -19,6 +28,33 @@ def send_cat(chat_id):
             'photo':url,
         }
     requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto',data=data1)
+
+def send_cat_and_dog(chat_id):
+    img_url = 'https://storage.googleapis.com/petbacker/images/blog/2017/dog-and-cat-cover.jpg'
+    data = {
+        'chat_id':chat_id,
+        'photo':img_url
+        
+        }
+    
+    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto',data=data)
+    
+
+
+
+
+def send_alternative(chat_id,text):
+    data2 = {
+        'chat_id':chat_id,
+        'text':'Iltimos dog yoki cat so`zini kiriting!'
+        }
+    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',data=data2)
+    
+
+
+
+
+
 def get_lastupdate(updates):
     last_update = updates['result'][-1]
     chat_id = last_update['message']['chat']['id']
@@ -26,40 +62,26 @@ def get_lastupdate(updates):
     message_id = last_update['message']['message_id']
     return chat_id,message_id,text
 
-# def urls(TOKEN,cat_data,dog_data,text,chat_id):
-#     url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
-#     dog = 'dog' or 'DOG' or 'Dog'
-#     cat = 'cat' or 'CAT' or 'Cat'
-#     cat_url = cat_data.get('file')
-    
-#     if text in dog:
-#         img_url = dog_data.get('url')
-#     if text in cat:
-#         cat_url = cat_data.get('file')
-#     if text not in dog or cat:
-#         text = 'Iltimos Dog yoki Cat yozing'
-    
 
-#     data = {
-#             'chat_id':chat_id,
-#             'photo':img_url,
-#             'text':text
-#         }
-#     data1 = {
-#             'chat_id':chat_id,
-#             'photo':cat_url,
-#             'text':text
-#         }
-    
-#     Dog = requests.post(url,data=data)
-#     Cat = requests.post(url,data=data1)
 new_message=-1
+
+
+
+
+
 while True:
     updates = get_updates(TOKEN)
     lastupdate = get_lastupdate(updates)
     chat_id,last_message_id,text= lastupdate
 
     if new_message != last_message_id:
+        if text.lower()=='cat and dog':
+            send_cat_and_dog(chat_id)
         if text.lower()=='cat':
             send_cat(chat_id)
+        elif text.lower()=='dog':
+            send_dog(chat_id)
+        elif text.lower() != 'cat' or 'dog':
+            send_alternative(chat_id,text)
+        
         new_message = last_message_id
